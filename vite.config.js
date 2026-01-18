1ecfd0092562e7cc65986c92cdfeab72978f7941
AJITHREDDY478 <ajithreddy478@gmail.com>
2026-01-18 18:12:57 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ajithreddy-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
